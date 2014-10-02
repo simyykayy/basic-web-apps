4c3ffa852db468622014428f4989241f1d814f8c
Simran Kumar <simyykayy@yahoo.com>
2014-10-02 23:12:11 +0000

    messed up my app

--- bake-app.rb
+++ bake-app.rb
@@ -3,13 +3,12 @@ require "sinatra"     # Load the Sinatra web framework
 get("/") do
   html = ""
 
-  html.concat("<h1>Hello, World!</h1>")
+  html.concat("<h1> Mission Bit!</h1>")
   html.concat("<ul>")
-  html.concat("<li><a href='/waffles'>show me waffles</a></li>")
-  html.concat("<li><a href='/waffles/chocolate'>show me chocolate</a></li>")
-  html.concat("<li><a href='/bake?baked_good=cookies&count=10'>bake 10 cookies</a></li>")
-  html.concat("<li><a href='/bake?baked_good=cronut&count=5'>bake 5 cronuts</a></li>")
-  html.concat("<li><a href='/bake?baked_good=cupcakes&count=1138'>bake 1138 cupcakes</a></li>")
+  html.concat("<li><a href='/waffles'>The location? </a></li>")
+  html.concat("<li><a href='/waffles/chocolate'> The students</a></li>")
+  html.concat("<li><a href='/bake?baked_good=cookies&count=10'> What is Mission Bit?</a></li>")
+  html.concat("<li><a href='/bake?baked_good=cronut&count=5'>Is it cool?</a></li>")
   html.concat("</ul>")
 
   body(html)
@@ -18,7 +17,7 @@ end
 get("/waffles") do
   html = ""
 
-  html.concat("<h1>Waffles are delicious.</h1>")
+  html.concat("<h1>Mission High School. </h1> <i> It's right across the street from Dolores Park.</i>")
 
   body(html)
 end
@@ -26,24 +25,17 @@ end
 get("/waffles/chocolate") do
   html = ""
 
-  html.concat("<h1>Chocolate waffles: more delicious.</h1>")
-  html.concat("<p>Don't believe me?!</p>")
+  html.concat("<h1> The students are the cool kids from SFUSD high schools. </h1>")
+  html.concat("<p><i>Don't believe me?! Go check them out.</i></p>")
 
   body(html)
 end
 
-# Visit, e.g., /bake?baked_good=waffles&count=20
 get("/bake") do
-  count      = Integer(params["count"])
-  baked_good = String(params["baked_good"])
 
-  html = "I'm going to bake #{count} #{baked_good}!"
+  html = "It's an afterschool computer coding program for middle/high school students."
 
   html.concat("<ul>")
-  count.times do |num|
-    html.concat("<li>#{baked_good} number #{num}</li>")
-  end
-  html.concat("</ul>")
 
   body(html)
 end
